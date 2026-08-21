d8780f4e1f51308f1f9a8a5b540b1bbb49875c2d
Phat Huynh <you@example.com>
2026-08-21 09:34:31 +0700

    Buoi1-Task6: Tạo bảng/migration ban đầu và kiểm tra DB khởi tạo thành công.

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
-from app.db.database import Base, engine
-
-app = FastAPI(
-    title="Marketing Campaign Management"
-    )
+from app.db.database import engine, Base
+from app.models.user import User
+from app.models.campaign import Campaign, CampaignMember, CampaignTask
 
 Base.metadata.create_all(bind=engine)
+
+app = FastAPI(title="Marketing Campaign Management API")
+
+@app.get("/")
+def root():
+    return {"message": "Chào mừng đến với Marketing Campaign Management API!"}
