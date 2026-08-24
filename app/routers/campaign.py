3564c499b2b072ba37a06237e871745a09df48ed
Phat Huynh <you@example.com>
2026-08-25 00:14:23 +0700

    Buoi3-Task2: GET /campaigns: chỉ trả chiến dịch mà user là owner/member; hỗ trợ search tên chiến dịch.

--- app/routers/campaign.py
+++ app/routers/campaign.py
@@ -1,11 +1,12 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query
 from sqlalchemy.orm import Session
 from datetime import datetime, timezone
 from app.db.database import get_db
 from app.models.user import User
-from app.models.campaign import Campaign
+from app.models.campaign import Campaign, CampaignMember
 from app.dependencies.auth import get_current_user
 from app.schemas.campaign import CampaignCreate, CampaignResponse
+from app.core.exceptions import (BadRequestException, ForbiddenException, NotFoundException)
 
 router = APIRouter(
     prefix="/campaigns",
@@ -13,11 +14,7 @@ router = APIRouter(
 )
 
 @router.post("", response_model=CampaignResponse)
-def create_campaign(
-    campaign_in: CampaignCreate,
-    db: Session = Depends(get_db),
-    current_user: User = Depends(get_current_user)
-):
+def create_campaign(campaign_in: CampaignCreate, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
     new_campaign = Campaign(
         name=campaign_in.name,
         description=campaign_in.description,
@@ -31,3 +28,16 @@ def create_campaign(
     new_campaign.owner = current_user
 
     return new_campaign
+
+@router.get("", response_model=list[CampaignResponse])
+def list_campaigns(db: Session = Depends(get_db), current_user: User = Depends(get_current_user), name: str | None = Query(None)):
+    query = db.query(Campaign).filter(Campaign.owner_id == current_user.id)
+    member_campaigns = (db.query(Campaign).join(CampaignMember).filter(CampaignMember.user_id == current_user.id))
+
+    campaigns = query.union(member_campaigns)
+
+    if name:
+        campaigns = campaigns.filter(Campaign.name.ilike(f"%{name}%"))
+
+    result = campaigns.all() 
+    return result
